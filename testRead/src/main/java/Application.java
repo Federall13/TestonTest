1aca3996c28236447fad16ac018b0eb550c068d2
Константин <k.vovk@lipt-soft.ru>
2020-02-17 22:12:57 +0300

    * Удалил ненужные строки

--- testRead/src/main/java/Application.java
+++ testRead/src/main/java/Application.java
@@ -4,9 +4,5 @@ public class Application{
     public static void main(String[] args) {
         int a = 5;
         System.out.println(a);
-        System.out.println("Если делаешь директорию src - тогда в ней делай main.java. Я это исправил.");
-        System.out.println("И gitignore я тебе сказал поместить в корневую папку.");
-        System.out.println("Корневая папка это папка в которой весь проект лежит в твоем случае - TestonTest");
-        System.out.println("Это я тоже исправил");
     }
 }
