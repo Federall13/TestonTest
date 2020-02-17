4c8a36176336fc9f1256cbd73f3eb7bc9dbe6172
Константин <k.vovk@lipt-soft.ru>
2020-02-17 19:34:09 +0300

    * Перенес gitignore * Добавил правиьную структуру файлов * Добавил TestonTest.iml в gitignore

--- testRead/src/Application.java
+++
@@ -1,8 +0,0 @@
-public class Application{
-    public static void main(String[] args) {
-        int a = 5;
-        System.out.println(a);
-        
-
-    }
-}
---
+++ testRead/src/main/java/Application.java
@@ -0,0 +1,10 @@
+public class Application{
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(a);
+        System.out.println("Если делаешь директорию src - тогда в ней делай main.java. Я это исправил.");
+        System.out.println("И gitignore я тебе сказал поместить в корневую папку.");
+        System.out.println("Корневая папка это папка в которой весь проект лежит в твоем случае - TestonTest");
+        System.out.println("Это я тоже исправил");
+    }
+}
