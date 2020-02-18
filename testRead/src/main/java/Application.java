229ba928331a0a9093d53419ee6890f230523a0d
Federall <bender92@list.ru>
2020-02-18 10:54:10 +0300

    Добавление в метод перемннной

--- testRead/src/main/java/Application.java
+++ testRead/src/main/java/Application.java
@@ -3,6 +3,7 @@ package main.java;
 public class Application{
     public static void main(String[] args) {
         int a = 5;
-        System.out.println(a);
+        int c = 10;
+        System.out.println(a + c);
     }
 }
